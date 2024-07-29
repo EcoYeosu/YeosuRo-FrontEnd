414c2ce204e2495eb7e75973355e14d29dda475c
ParkSeungWoo <qpfgud430@gmail.com>
2024-07-29 15:43:07 +0900

    feat: formatDate 및 useInput 커스텀 훅 구현

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,13 @@
+import { ChangeEvent, useState } from "react";
+
+export const useInput = (initialValue: string) => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+  };
+
+  const reset = () => setValue('');
+
+  return { value, handleChange, reset };
+};
