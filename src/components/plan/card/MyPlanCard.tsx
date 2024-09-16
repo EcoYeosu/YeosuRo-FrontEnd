c56a3f0607a131fde1b6946ad3c2007febd5faf4
rlaxotn2300 <rlaxotn2300@naver.com>
2024-09-16 21:14:28 +0900

    바텀시트 오류 해결, api응답 받을준비 완

--- src/components/plan/card/MyPlanCard.tsx
+++ src/components/plan/card/MyPlanCard.tsx
@@ -2,12 +2,23 @@ import { CardProps } from '@/type/plan'
 
 const MyPlanCard: React.FunctionComponent<CardProps> = ({title,startDate,endDate,editPage}) => {
     return (
-        <div style={{ width: '320px', height:'100px', background:'#d9d9d9', borderRadius:'8px', position:'relative',padding:'20px'}} onClick={editPage}>
-            <div style={{height:'100%',display:'flex', flexDirection:'column', justifyContent:'center'}}>
+        <div style={{ width: '320px', height:'100px', display:'flex',alignItems:'center'}} onClick={editPage}>
+            <div style={{width:'243px',height:'100%',display:'flex', flexDirection:'column', justifyContent:'center',paddingLeft:'20px',background:'#d9d9d9',borderRadius:'8px'}}>
                 <p style={{ fontWeight:'600', fontSize:'20px'}}>{title ? title : '혜지랑 1박2일'}</p>
                 <p style={{ color:'#777777',fontWeight:'400', fontSize:'16px' }}>{startDate ? startDate : `2024. 05. 04`} ~ {endDate ? endDate : `2024. 05. 06`}</p>
             </div>
-                <button style={{ color:'#0D77E0',position:'absolute',right:'14px',bottom:'14px', fontWeight:'400', fontSize:'14px' }}>편집</button>
+            <div style={{height:'83px',border:'1px dashed black'}}/>
+            <button style={{
+                 color:'#0D77E0',
+                 fontWeight:'400',
+                 fontSize:'14px',
+                 width:'77px',
+                 height:'100px',
+                 borderRadius:'8px',
+                 background:'#d9d9d9',
+                }}>
+                편집
+            </button>
         </div>
     )
 };
--- src/hooks/plan.ts
+++ src/hooks/plan.ts
@@ -15,3 +15,4 @@ export const useGetPlanList = () => {
     return response
   }})
 }
+
